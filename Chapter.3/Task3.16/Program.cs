2f4f665635349cbda6c336c48f59a2ca81943309
Slavena Georgieva <44895751+slawenna@users.noreply.github.com>
2018-11-11 13:11:08 +0200

    Update Program.cs

--- Chapter.3/Task3.16/Program.cs
+++ Chapter.3/Task3.16/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,147 +8,143 @@ namespace Task3._16
 {
     class Program
     {
-        static void Main(string[] args)
+        private static uint ModifyNumber(uint number, int p, int q, int k)
+
         {
-            private static uint ModifyNumber(uint number, int p, int q, int k)
+
+            int[] pBits = new int[k];
+
+            int[] qBits = new int[k];
+
+
+
+            for (int position = p, i = 0; i < pBits.Length; position++, i++)
 
             {
 
-                int[] pBits = new int[k];
-
-                int[] qBits = new int[k];
-
-
-
-                for (int position = p, i = 0; i < pBits.Length; position++, i++)
-
-                {
-
-                    pBits[i] = PthBit(number, position);
-
-                }
-
-
-
-                for (int position = q, i = 0; i < qBits.Length; position++, i++)
-
-                {
-
-                    qBits[i] = PthBit(number, position);
-
-                }
-
-
-
-                for (int position = p, i = 0; i < qBits.Length; position++, i++)
-
-                {
-
-                    number = ModifiedNumber(number, position, qBits[i]);
-
-                }
-
-
-
-                for (int position = q, i = 0; i < pBits.Length; position++, i++)
-
-                {
-
-                    number = ModifiedNumber(number, position, pBits[i]);
-
-                }
-
-                return number;
+                pBits[i] = PthBit(number, position);
 
             }
 
 
 
-            private static int PthBit(uint number, int position)
+            for (int position = q, i = 0; i < qBits.Length; position++, i++)
 
             {
 
-                uint pthBit = (number >> position) & 1;
-
-                return (int)pthBit;
+                qBits[i] = PthBit(number, position);
 
             }
 
 
 
-            private static uint ModifiedNumber(uint number, int position, int bitValue)
+            for (int position = p, i = 0; i < qBits.Length; position++, i++)
 
             {
 
-                uint actualP = (uint)bitValue << position;
-
-                number = number & (~((uint)1 << position));
-
-                uint result = number | actualP;
-
-                return result;
+                number = ModifiedNumber(number, position, qBits[i]);
 
             }
 
 
 
-            static void Main(string[] args)
+            for (int position = q, i = 0; i < pBits.Length; position++, i++)
 
             {
 
-                Console.Write("Enter number: ");
-
-                uint number = uint.Parse(Console.ReadLine());
-
-                Console.Write("Enter p: ");
-
-                int p = int.Parse(Console.ReadLine());
-
-                Console.Write("Enter q: ");
-
-                int q = int.Parse(Console.ReadLine());
-
-                Console.Write("Enter k: ");
-
-                int k = int.Parse(Console.ReadLine());
-
-
-
-
-
-                if (p > q)
-
-                {
-
-                    int oldValue = p;
-
-                    p = q;
-
-                    q = oldValue;
-
-                }
-
-
-
-                if (p + k >= q)
-
-                {
-
-                    k += p - q - 1;
-
-                    q += p + k + 1;
-
-                }
-
-
-
-                number = ModifyNumber(number, p, q, k);
-
-
-
-                Console.WriteLine(number);
+                number = ModifiedNumber(number, position, pBits[i]);
 
             }
+
+            return number;
+
+        }
+
+
+
+        private static int PthBit(uint number, int position)
+
+        {
+
+            uint pthBit = (number >> position) & 1;
+
+            return (int)pthBit;
+
+        }
+
+
+
+        private static uint ModifiedNumber(uint number, int position, int bitValue)
+
+        {
+
+            uint actualP = (uint)bitValue << position;
+
+            number = number & (~((uint)1 << position));
+
+            uint result = number | actualP;
+
+            return result;
+
+        }
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            Console.Write("Enter number: ");
+
+            uint number = uint.Parse(Console.ReadLine());
+
+            Console.Write("Enter p: ");
+
+            int p = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter q: ");
+
+            int q = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter k: ");
+
+            int k = int.Parse(Console.ReadLine());
+
+
+
+
+
+            if (p > q)
+
+            {
+
+                int oldValue = p;
+
+                p = q;
+
+                q = oldValue;
+
+            }
+
+
+
+            if (p + k >= q)
+
+            {
+
+                k += p - q - 1;
+
+                q += p + k + 1;
+
+            }
+
+
+
+            number = ModifyNumber(number, p, q, k);
+
+
+
+            Console.WriteLine(number);
         }
     }
 }
